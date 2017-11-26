42e70efd38735775caf2e40b2cd1d718525e78f0
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 03:19:18 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,5 +8,13 @@ function lastImageSelector(){
 }
 
 function ninjaBabySelector(){
-  return $(#)
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul #pic-list:first-child')
 }
